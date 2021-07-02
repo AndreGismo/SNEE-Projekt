be8b8952f7cc2655db0f83815f06858fc1234534
Andre Ulrich <andre.ulrich63920@gmail.com>
2021-07-02 16:25:36 +0200

    datasource bei ControllableBattery jetzt als Klassen-Attribut, außerdem Fehler bei Start des Ladevorgangs um 00:00 abgefangen

--- controllable_battery.py
+++ controllable_battery.py
@@ -15,13 +15,9 @@ nachgehalten werden)
 import numpy as np
 
 class ControllableBattery:
-    #TODO
-    # die ControllableBattery muss ohne controller erzeugt werden (weil zum
-    # Zeitpunkt der Erzeugung noch gar kein Controller existiert - und wenn
-    # der Kontroller dann erzeugt wird, dann brauch er die Batterien alle)
-    # => methode add_controller, die aufgerufen wird, sobald der controller
-    # existiert
-    # vielleicht einfach nur die datasource vom Controller hinzufügen?
+    
+    __data_source = None
+    
     def __init__(self, net, at_load, according_bus, energy, power,
                  distance_travelled, consumption, arrival, u_ls=4.2, u_n=3.6,
                  i_ls=0.3, s=80):
@@ -37,24 +33,30 @@ class ControllableBattery:
         self.u_n = u_n
         self.i_ls = i_ls
         self.s = s
-        self.data_source = None
-        
-    # Überflüssig   
-    def register_controller(self, controller):
-        self.controller = controller
         
         
     def register_datasource(self, datasource):
-        self.data_source = datasource
+        type(self).data_source = datasource
         
         
     def calc_soc(self, timestep):
+        # solange die arrival time noch nicht erreicht worden ist, geschieht
+        # noch nichts
         if timestep < self.arrival_time:
             pass
+        
         else:
-            self.current_energy += self.data_source.df.at\
-                [timestep-1, self.at_load]/4*1000
-            self.current_soc = self.current_energy/self.energy*100
+            # für den Fall, dass ein Auto ab 00:00 beginnt zu laden
+            if timestep == 0: #neu
+                self.current_energy += 0 #neu
+                self.current_soc = self.current_energy/self.energy*100 # neu
+                
+            else: #neu
+                # ansonsten ist die Energie gleich der Energie des letzten
+                # Zeitpunkts + der Leistung des letzten Zeitpunkts
+                self.current_energy += self.data_source.df.at\
+                    [timestep-1, self.at_load]/4*1000
+                self.current_soc = self.current_energy/self.energy*100
         
     
     def calc_power(self, timestep):
@@ -80,8 +82,8 @@ class ControllableBattery:
     def _write_to_controller_ds(self, timestep):
         self.calc_soc(timestep)
         self.calc_power(timestep)
-        self.print_interest(timestep)
-        self.data_source.df.at[timestep, self.at_load] = self.current_power/1000
+        #self.print_interest(timestep)
+        type(self).data_source.df.at[timestep, self.at_load] = self.current_power/1000
     
     
     def adapt_power(self, du):
